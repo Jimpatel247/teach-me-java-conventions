a2b8d26d9b978cf60b48098c956eca8f94969c8f
jimpatel247 <pateljim2002@gmail.com>
2024-07-26 21:42:28 +0530

    Implement project structure and update package declarations as per README requirements

---
+++ src/test/java/powerpackage/PowerTest.java
@@ -0,0 +1,28 @@
+
+
+package powerpackage;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class PowerTest {
+    @Test
+    public void oneRaisedToOneIsOne() {
+        assertEquals(1, PowerFinder.of(1, 1));
+    }
+
+    @Test
+    public void twoRaisedToOneIsTwo() {
+        assertEquals(2, PowerFinder.of(2, 1));
+    }
+
+    @Test
+    public void twoPowerTwoIsFour() {
+        assertEquals(4, PowerFinder.of(2, 2));
+    }
+
+    @Test
+    public void threeRaisedToTwoIsNine() {
+        assertEquals(9, PowerFinder.of(3, 2));
+    }
+}
